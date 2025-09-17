80c902880e23daf768eb3a77d493c744124ad627
ritz51 <rithanyama@gmail.com>
2025-09-17 18:03:04 +0530

    division

--- main.cpp
+++ main.cpp
@@ -8,12 +8,18 @@ int sub(int a,int b)
 {
     return a-b;
 }
+float div(int a,int b)
+{
+    return a/b;
+}
 int main()
 {
     int a,b,res,res2;
+    float resd;
     cin>>a;
     cin>>b;
     res=add(a,b);
     res2=sub(a,b);
+    resd=div(a,b);
     return 0;
 }
