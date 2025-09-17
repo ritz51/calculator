524a508e3c33acb48e3c52f9bcb1493f37f6a9e0
ritz51 <rithanyama@gmail.com>
2025-09-17 17:36:46 +0530

    addition

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int add(int a,int b)
+{
+    return a+b;
+}
+int main()
+{
+    int a,b,res;
+    cin>>a;
+    cin>>b;
+    res=add(a,b);
+    return 0;
+}
